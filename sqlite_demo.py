2b74bf8e4c77f8ea917bc3ee41fb313b8cc8e07c
borchr27 <30349201+borchr27@users.noreply.github.com>
2020-01-15 12:09:52 -0500

    Practicing manipulating databases

--- sqlite_demo.py
+++ sqlite_demo.py
@@ -4,16 +4,9 @@ from employee import Employee
 
 # Import file or in memory (':memory:') database (fresh clean database on every run)
 #conn = sqlite3.connect('employee.db')
-conn = sqlite3.connect(':memory:')
+conn = sqlite3.connect('employee.db')
 
 c = conn.cursor()
-
-#Creates db table
-c.execute("""CREATE TABLE employees (
-    first text,
-    last text,
-    pay integer
-    )""")
 
 def insert_emp(emp):
     with conn: #closes the connection w the db when finished
@@ -31,44 +24,26 @@ def remove_emp(emp):
     with conn:
         c.execute("DELETE from employees WHERE first = :first AND last = :last", {'first': emp.first, 'last': emp.last})
 
+def show_all():
+    with conn:
+        c.execute("SELECT * FROM employees")
+    return print(c.fetchall()) 
+
 # Pyhton objects not yet inserted into the database
-emp_1 = Employee('John', 'Doe', 80000)
-emp_2 = Employee('Jane', 'Doe', 87000)
+emp_1 = Employee('First', 'Last', 55000)
 
-insert_emp(emp_1)
-insert_emp(emp_2)
+#remove_emp(emp_1)
+#insert_emp(emp_1)
 
-emps = get_emps_by_name('Doe')
-print(emps)
+#emps = get_emps_by_name('Doe')
+#print(emps)
 
-update_pay(emp_2, 95700)
-remove_emp(emp_1)
+#update_pay(emp_2, 95700)
+#remove_emp(emp_1)
 
-emps = get_emps_by_name('Doe')
-print(emps)
+#emps = get_emps_by_name('Doe')
+#print(emps)
+
+show_all()
 
 conn.close()
-
-def notes():
-    """
-    # Manual code to modify db
-    c.execute("INSERT INTO employees VALUES (?,?,?)", (emp_1.first, emp_1.last, emp_1.pay))
-    conn.commit()
-
-    c.execute("INSERT INTO employees VALUES (:first, :last, :pay)", {'first': emp_2.first, 'last': emp_2.last, 'pay': emp_2.pay})
-    conn.commit()
-
-    # Insert data statement
-    c.execute("INSERT INTO employees VALUES ('Corey', 'Schafer', 47000)")
-
-    # Looking for employee in database
-    c.execute("SELECT * FROM employees WHERE last=?", ('Schafer',))
-    print(c.fetchall())
-
-    c.execute("SELECT * FROM employees WHERE last=:last", {'last': 'Doe'})
-    print(c.fetchall())
-
-    conn.commit()
-    conn.close()
-    """
-
