96312630e44f982c1b7225a7bf932ceb41c661ed
borchr27 <30349201+borchr27@users.noreply.github.com>
2020-01-19 22:40:24 -0500

    Creating more powerful recipe search function

--- recipe_demo.py
+++ recipe_demo.py
@@ -9,6 +9,7 @@ conn_d = sqlite3.connect('directions.db')
 c_i = conn_i.cursor()
 c_d = conn_d.cursor()
 
+
 def insert_ingredient(item):
     with conn_i: #closes the connection w the db when finished
         c_i.execute("INSERT INTO ingredients VALUES (:recipe, :ingredient, :amt )", {'recipe': item.recipe, 'ingredient': item.ingredient, 'amt': item.amt})
@@ -27,9 +28,15 @@ def show_directions_table():
         c_d.execute("SELECT * FROM directions")
     return print(c_d.fetchall())
 
-x = 'choco'
-c_d.execute("SELECT * FROM directions WHERE recipe LIKE '%{}%' ".format(x))
-print(c_d.fetchall())
+x = input("Search for recipes that have these ingredients: ")
+# High probability search relevance (search: egg, return: egg)
+c_i.execute("SELECT * FROM ingredients WHERE ingredient LIKE '{}' ".format(x))
+# Low + high probability search relevance (search: egg, return: egg, veggies, eggplant)
+c_i.execute("SELECT * FROM ingredients WHERE ingredient LIKE '%{}%' ".format(x))
+
+#c_d.execute("SELECT recipe FROM directions WHERE recipe LIKE '%{}%' ".format(x))
+print(c_i.fetchall())
+#print(c_d.fetchall())
 
 #show_directions_table()
 #show_ingredients_table()
